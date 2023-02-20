a366ba364afa3d6d01569cdc29bfffc8d33283af
Michael <michael@arlanet.com>
2023-02-20 10:56:18 +0100

    netlify redirect test

--- pages/login.vue
+++ pages/login.vue
@@ -5,7 +5,7 @@
       class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
       @click="login"
     >
-      Log in with Github
+      Log in with Github - test
     </button>
   </div>
 </template>
